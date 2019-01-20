3fa53ce3fc142517c9bd24ec03872b271d2a9257
Lin Song Yang <linsongyang@gmail.com>
2019-01-20 14:03:37 +1100

    added missing dataset split and feature scaling

--- day-1/day-1.py
+++ day-1/day-1.py
@@ -55,3 +55,16 @@ from sklearn.preprocessing import OneHotEncoder
 onehotencoder = OneHotEncoder(categorical_features = [0])
 X = onehotencoder.fit_transform(X).toarray()
 print(X)
+
+# split dataset 80:20 training:test
+# from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split( X , Y , test_size = 0.2, random_state = 0)
+
+# feature scling
+# The StandardScaler assumes your data is normally distributed within each feature and will scale them
+# such that the distribution is now centred around 0, with a standard deviation of 1
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.fit_transform(X_test)
