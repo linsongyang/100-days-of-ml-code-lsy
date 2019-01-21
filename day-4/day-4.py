bd9162d44ddf438287f9646c03d4bd2197d06d72
Lin Song Yang <linsongyang@gmail.com>
2019-01-21 21:47:30 +1100

    classification visualization

--- day-4/day-4.py
+++ day-4/day-4.py
@@ -14,11 +14,15 @@
 
 import numpy as np # numpy
 import pandas as pd # pandas dataset manipulation
+from pandas import DataFrame
 import os # io
+from string import ascii_uppercase
 from sklearn.model_selection import train_test_split # train set
 from sklearn.preprocessing import StandardScaler # feature scaling
 from sklearn.linear_model import LogisticRegression # log regression
 from sklearn.metrics import confusion_matrix # matrix
+from matplotlib import pyplot as plt
+import seaborn as seaborn
 
 # load the csv file to dataset
 # contains columns: User ID,Gender,Age,EstimatedSalary,Purchased
@@ -39,12 +43,51 @@ X_train = scaler.fit_transform(X_train)
 X_test = scaler.fit_transform(X_test)
 
 # apply log regression
-regressor = LogisticRegression()
-regressor = regressor.fit(X_train, Y_train)
+classifier = LogisticRegression()
+classifier = classifier.fit(X_train, Y_train)
 
 # predication
-Y_pred = regressor.predict(X_test)
+Y_pred = classifier.predict(X_test)
 print(Y_pred)
 
 matrix = confusion_matrix(Y_test, Y_pred)
-print(matrix)
+#indexes = ['test %s' %(i) for i in list(ascii_uppercase)[0:len(np.unique(Y_test))]]
+#columns = ['pred %s' %(i) for i in list(ascii_uppercase)[0:len(np.unique(Y_pred))]]
+#dataFrame = pd.DataFrame(matrix, index=indexes, columns=columns)
+
+# visualization
+from matplotlib.colors import ListedColormap
+X_set,y_set=X_train,Y_train
+X1,X2=np. meshgrid(np. arange(start=X_set[:,0].min()-1, stop=X_set[:, 0].max()+1, step=0.01),
+                   np. arange(start=X_set[:,1].min()-1, stop=X_set[:,1].max()+1, step=0.01))
+plt. contourf(X1, X2, classifier.predict(np.array([X1.ravel(),X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt. xlim(X1.min(),X1.max())
+plt. ylim(X2.min(),X2.max())
+for i,j in enumerate(np. unique(y_set)):
+    plt. scatter(X_set[y_set==j,0],X_set[y_set==j,1],
+                c = ListedColormap(('red', 'green'))(i), label=j)
+
+plt. title(' LOGISTIC(Training set)')
+plt. xlabel(' Age')
+plt. ylabel(' Estimated Salary')
+plt. legend()
+plt. show()
+
+X_set,y_set=X_test,Y_test
+X1,X2=np. meshgrid(np. arange(start=X_set[:,0].min()-1, stop=X_set[:, 0].max()+1, step=0.01),
+                   np. arange(start=X_set[:,1].min()-1, stop=X_set[:,1].max()+1, step=0.01))
+
+plt. contourf(X1, X2, classifier.predict(np.array([X1.ravel(),X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt. xlim(X1.min(),X1.max())
+plt. ylim(X2.min(),X2.max())
+for i,j in enumerate(np. unique(y_set)):
+    plt. scatter(X_set[y_set==j,0],X_set[y_set==j,1],
+                c = ListedColormap(('red', 'green'))(i), label=j)
+
+plt. title(' LOGISTIC(Test set)')
+plt. xlabel(' Age')
+plt. ylabel(' Estimated Salary')
+plt. legend()
+plt. show()
